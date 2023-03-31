6a76256f7974ccc5e9f80516bc3648588b9057fe
Samuele <samuele.quinzi@uniroma3.it>
2023-03-31 19:41:30 +0200

    Our model builder

--- model_builder.py
+++ model_builder.py
@@ -9,52 +9,30 @@ from keras.layers import (
     BatchNormalization
 )
 from keras.models import Model
+from keras.models import Sequential 
 
 
-class TextSegmentationModelBuilder:
+class ScalogramSegmentationLSTMModelBuilder:
     def __init__(
         self,
-        embedding_layer: Layer,
-        n_sentencewise_lstm_layers: int,
-        n_documentwise_lstm_layers: int,
-        lstm_dimension: int,
+        window_size: int,
+        dwt_levels : int,
     ):
-        """
-        :param embedding_layer: Keras embedding layer. 
-            The layer is passed as argument, because the merge of pre-trained and custom vocab 
-            for building the embedding matrix is managed by the training script.
-        :param n_sentencewise_lstm_layers: Number of LSTM layers for the word-level network
-        :param n_documentwise_lstm_layers: Number of LSTM layers for the sentence-level network
-        :param lstm_dimension: Dimension (units) of LSTM layers
-        """
-
-
-        self.embedding_layer = embedding_layer
-        self.lstm_dimension = lstm_dimension
-        self.n_sentencewise_lstm_layers = n_sentencewise_lstm_layers
-        self.n_documentwise_lstm_layers = n_documentwise_lstm_layers
+        self.window_size = window_size
+        self.dwt_levels = dwt_levels
 
     def build_network(self):
-        sentences = Input(shape=(None, None))
+        scalogram_fragments = (self.window_size, self.dwt_levels)
 
-        x = self.embedding_layer(sentences)
-
-        # Word-level LSTM
-        for layer in range(self.n_sentencewise_lstm_layers):
-            x = TimeDistributed(
-                Bidirectional(LSTM(
-                    units=self.lstm_dimension,
-                    return_sequences=True
-                ))
-            )(x)
-
-        x = TimeDistributed(GlobalMaxPool1D())(x)
-
-        # Sentence-level LSTM
-        for layer in range(self.n_documentwise_lstm_layers):
-            x = Bidirectional(LSTM(units=self.lstm_dimension, return_sequences=True))(x)
-
-        x = TimeDistributed(Dense(1, activation="sigmoid"))(x)
-
-        model = Model(sentences, x)
+        lstm_layer = LSTM(
+                    units=1,
+                    return_sequences=True,
+                    input_shape = scalogram_fragments
+                )
+        model = Sequential()
+        model.add(lstm_layer)
         return model
+
+builder = ScalogramSegmentationLSTMModelBuilder(1000, 15)
+m = builder.build_network()
+print(m.summary())
